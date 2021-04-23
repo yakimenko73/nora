210ca3300aea4fb18505d1ef1eba2387d138e9f6
m.levkin <m.levkin@simcord.com>
2021-04-23 16:50:40 +0300

    wip

--- main.go
+++ main.go
@@ -3,20 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/streadway/amqp"
 	"load-testing/config"
 	"load-testing/core/dispatcher"
 	"load-testing/load"
 	"os"
+	"time"
 )
 
 var (
 	A int = 0
+
+	conn *amqp.Connection
+	channel *amqp.Channel
 )
 
 func prinSmth() error {
 	A++
 
+	//return channel.Publish(
+	//	"amq.direct",
+	//	"r.test",
+	//	false,
+	//	false,
+	//	amqp.Publishing{
+	//		Body: []byte("privet"),
+	//	},
+	//)
 	return nil
+}
+
+func init() {
+	var err error
+	conn, err = amqp.Dial("amqp://admin:admin@localhost:5772/")
+	if err != nil {
+		panic(err)
+	}
+
+	channel, err = conn.Channel()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -32,7 +59,9 @@ func main() {
 	ls.SetLoadTime(cfg.TestDuration)
 	ls.AddJob(prinSmth)
 
+	fmt.Println(time.Now())
 	ls.Start()
-	
-	fmt.Println(A)
+	fmt.Println(time.Now())
+
+	fmt.Println(float32(A) / 1000000)
 }
