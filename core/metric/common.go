19ddae260ff9e85faa111709e985e3c147ab09ca
m.levkin <m.levkin@simcord.com>
2021-07-01 15:37:01 +0300

    add metrics calculation

--- core/metric/common.go
+++ core/metric/common.go
@@ -10,3 +10,18 @@ type Result struct {
 
 	Error           error
 }
+
+type LatencyMetrics struct {
+	Total time.Duration
+
+	Min time.Duration
+	Max time.Duration
+
+	Q1 time.Duration
+	Q3 time.Duration
+	Median time.Duration
+
+	P90 time.Duration
+	P95 time.Duration
+	P99 time.Duration
+}
--- core/metric/metrics.go
+++ core/metric/metrics.go
@@ -1,5 +1,10 @@
 package metric
 
+import (
+	"sort"
+	"time"
+)
+
 type Metrics struct {
 	Results map[string][]*Result
 }
@@ -14,4 +19,46 @@ func (m *Metrics) ConsumeResult(result *Result) {
 	}
 
 	m.Results[result.Name] = append(m.Results[result.Name], result)
+}
+
+func (m *Metrics) GetLatencyMetrics() map[string]LatencyMetrics  {
+	latencies := make(map[string]LatencyMetrics, 0)
+	for job, metrics := range m.Results {
+
+		durations := make([]time.Duration, 0)
+		lMetrics := LatencyMetrics{
+			Min: -1,
+			Max: -1,
+			Total: 0,
+		}
+		for _, metric := range metrics {
+			lMetrics.Total += metric.Duration
+			if lMetrics.Min > metric.Duration || lMetrics.Min == -1 {
+				lMetrics.Min = metric.Duration
+			}
+			if lMetrics.Max < metric.Duration || lMetrics.Max == -1 {
+				lMetrics.Max = metric.Duration
+			}
+
+			SortedInsert(durations, metric.Duration)
+		}
+		lMetrics.Q1 = calculatePercentile(durations, 25)
+		lMetrics.Median = calculatePercentile(durations, 50)
+		lMetrics.Q3 = calculatePercentile(durations, 75)
+		lMetrics.P90 = calculatePercentile(durations, 90)
+		lMetrics.P95 = calculatePercentile(durations, 95)
+		lMetrics.P99 = calculatePercentile(durations, 99)
+
+		latencies[job] = lMetrics
+	}
+
+	return latencies
+}
+
+func SortedInsert(durations []time.Duration, duration time.Duration) []time.Duration {
+	i := sort.Search(len(durations), func(i int) bool {return durations[i] >= duration})
+	durations = append(durations, 0)
+	copy(durations[i+1:],durations[i:])
+	durations[i] = duration
+	return durations
 }
