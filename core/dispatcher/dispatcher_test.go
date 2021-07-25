d1041afb92a79d4fad6453ae892fd5b376d178cd
Maksim Levkin <aquamarineasf@gmail.com>
2021-07-26 00:32:24 +0300

    add simple tests

---
+++ core/dispatcher/dispatcher_test.go
@@ -0,0 +1,29 @@
+package dispatcher
+
+import (
+	"context"
+	"github.com/illatior/task-scheduler/core/executor"
+	"github.com/illatior/task-scheduler/core/task"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestCallDispatch(t *testing.T) {
+	t.Parallel()
+
+	sch := ConstantScheduler{
+		Frequency: 1,
+		Period:    1 * time.Millisecond,
+	}
+
+	taskk := task.NewBaseTask(func() error {
+		return nil
+	}, "task name")
+
+	exec := executor.New()
+	exec.AddTask(&taskk)
+
+	for range Dispatch(context.Background(), sch, exec, time.Second, uint64(runtime.GOMAXPROCS(0))) {
+	}
+}
---
+++ core/dispatcher/scheduler_test.go
@@ -0,0 +1,46 @@
+package dispatcher
+
+import (
+	"testing"
+	"time"
+)
+
+func TestConstantSchedulerNext(t *testing.T) {
+	t.Parallel()
+
+	for ti, td := range []struct {
+		freq    uint64
+		period  time.Duration
+		elapsed time.Duration
+		hits    uint64
+
+		next time.Duration
+		stop bool
+	} {
+		{1, time.Second, 0, 0, time.Second, false},
+
+		{1, time.Second, time.Second, 1, time.Second, false},
+
+		{1, time.Second, time.Second, 2, 2 * time.Second, false},
+
+		{1, time.Second, time.Second, 0, 0, false},
+
+		// Infinite
+		{1, 0, time.Second, 2, 0, false},
+		{0, time.Second, time.Second, 2, 0, false},
+
+		// Negative cases
+		{1, -1, time.Second, 0, 0, true},
+	} {
+		cs := ConstantScheduler{
+			Frequency: td.freq,
+			Period:    td.period,
+		}
+
+		next, stop := cs.GetNextExecution(td.elapsed, td.hits)
+		if next != td.next || stop != td.stop {
+			t.Errorf("Test %d: (%d, %s)\nExpected: (%s, %t)\nActual: (%s, %t)",
+				ti, td.freq, td.period, td.next, td.stop, next, stop)
+		}
+	}
+}
