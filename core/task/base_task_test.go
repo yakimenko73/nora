f97fa1ed0944415a549e4f4927bf38481ceee340
Maksim Levkin <aquamarineasf@gmail.com>
2021-10-03 01:46:05 +0300

    add test's for task

--- core/task/base_task_test.go
+++ core/task/base_task_test.go
@@ -34,3 +34,36 @@ func TestBaseTask_Run(t *testing.T) {
 	assert.GreaterOrEqual(t, res.Start.Sub(start), time.Duration(0))
 	assert.LessOrEqual(t, res.Start.Sub(start), delta)
 }
+
+func TestBaseTask_ContextCancelled(t *testing.T) {
+	t.Parallel()
+
+	start := time.Now()
+
+	delta := 100 * time.Millisecond
+	contextTimeout := 1 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	isHandledContextDone := false
+
+	taskName := "test"
+	taskFunc := func(ctx context.Context) error {
+		select {
+		case <-ctx.Done():
+			isHandledContextDone = true
+			return nil
+		case <-time.After(contextTimeout * 2):
+			return nil
+		}
+	}
+
+	taskk := NewBaseTask(taskFunc, taskName)
+	taskk.Run(ctx)
+
+	assert.True(t, isHandledContextDone)
+
+	since := time.Since(start)
+	assert.GreaterOrEqual(t, since, time.Duration(0))
+	assert.LessOrEqual(t, since, contextTimeout+delta)
+}
--- core/task/time_limited_task_test.go
+++ core/task/time_limited_task_test.go
@@ -1,1 +1,98 @@
 package task
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestTimeLimitedTask_Run(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	start := time.Now()
+
+	maxExecutionTime := 5 * time.Microsecond
+
+	taskName := "test task"
+	taskTimeout := 1 * time.Second
+
+	isExecuted := false
+	taskFunction := func(ctx context.Context) error {
+		isExecuted = true
+		return nil
+	}
+
+	taskk := NewTimeLimitedTask(taskFunction, taskName, taskTimeout)
+	res := taskk.Run(ctx)
+
+	assert.True(t, isExecuted)
+
+	since := time.Since(start)
+	assert.GreaterOrEqual(t, since, time.Duration(0))
+	assert.LessOrEqual(t, since, maxExecutionTime)
+
+	assert.Equal(t, res.Name, taskName)
+	assert.Nil(t, res.Error)
+
+	assert.GreaterOrEqual(t, res.Start.Sub(start), time.Duration(0))
+	assert.LessOrEqual(t, res.Start.Sub(start), since)
+
+	assert.GreaterOrEqual(t, res.Duration, time.Duration(0))
+	assert.LessOrEqual(t, res.Duration, since)
+}
+
+func TestTimeLimitedTask_TimeOut(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	start := time.Now()
+
+	maxExecutionTime := 5 * time.Microsecond
+
+	taskName := "test task"
+	taskTimeout := 1 * time.Second
+
+	isExecuted := false
+	taskFunction := func(ctx context.Context) error {
+		isExecuted = true
+		time.Sleep(taskTimeout + 50*time.Millisecond)
+		return nil
+	}
+
+	taskk := NewTimeLimitedTask(taskFunction, taskName, taskTimeout)
+	res := taskk.Run(ctx)
+
+	assert.True(t, isExecuted)
+
+	since := time.Since(start)
+	assert.GreaterOrEqual(t, since, time.Duration(0))
+	assert.LessOrEqual(t, since, maxExecutionTime)
+
+	assert.Equal(t, res.Name, taskName)
+	assert.Equal(t, res.Error, errTaskTimedOut)
+
+	assert.GreaterOrEqual(t, res.Start.Sub(start), time.Duration(0))
+	assert.LessOrEqual(t, res.Start.Sub(start), since)
+
+	assert.GreaterOrEqual(t, res.Duration, time.Duration(0))
+	assert.LessOrEqual(t, res.Duration, since)
+}
+
+func TestTimeLimitedTask_ContextCancelled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	taskFunction := func(ctx context.Context) error {
+		cancel()
+		time.Sleep(10 * time.Millisecond)
+
+		return nil
+	}
+
+	taskk := NewTimeLimitedTask(taskFunction, "", time.Minute)
+	res := taskk.Run(ctx)
+
+	assert.Equal(t, res.Error, errContextCancelled)
+}
